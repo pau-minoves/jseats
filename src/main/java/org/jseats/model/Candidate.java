cd6985946d29f0de9357506e89ef5b824e9e58ad
Pau Minoves <pau.minoves@gmail.com>
2014-12-12 19:20:13 +0100

    Candidate support for properties

--- src/main/java/org/jseats/model/Candidate.java
+++ src/main/java/org/jseats/model/Candidate.java
@@ -1,5 +1,9 @@
 package org.jseats.model;
 
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.StringTokenizer;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -17,14 +21,18 @@ public class Candidate implements Comparable<Candidate> {
 	@XmlTransient
 	boolean hasVotes;
 
+	Properties properties;
+
 	public Candidate() {
 		this.hasVotes = true;
+		properties = new Properties();
 	}
 
 	public Candidate(String name) {
 
 		this.name = name;
 		this.hasVotes = false;
+		properties = new Properties();
 	}
 
 	public Candidate(String name, int votes) {
@@ -32,6 +40,7 @@ public class Candidate implements Comparable<Candidate> {
 		this.name = name;
 		this.hasVotes = true;
 		this.votes = votes;
+		properties = new Properties();
 	}
 
 	public int getVotes() {
@@ -55,6 +64,20 @@ public class Candidate implements Comparable<Candidate> {
 		return hasVotes;
 	}
 
+	public String setProperty(String key, String value) {
+		if(value==null) {
+			String previous = properties.getProperty(key);
+			properties.remove(key);
+			return previous;
+		}
+			
+		return (String) properties.setProperty(key, value);
+	}
+
+	public String getProperty(String key) {
+		return properties.getProperty(key);
+	}
+
 	public void hasVotes(boolean hasVotes) {
 		this.hasVotes = hasVotes;
 	}
@@ -72,28 +95,46 @@ public class Candidate implements Comparable<Candidate> {
 		StringBuilder str = new StringBuilder(name);
 		str.append(":");
 		str.append(votes);
+		
+		if(!properties.isEmpty())
+		{
+			Iterator<Object> i = properties.keySet().iterator();
+			
+			while (i.hasNext()) {
+				String key = (String) i.next();
+				str.append(":" + key + "=" + properties.getProperty(key));
+			}
+				
+		}
+
+		
 		return str.toString();
 	}
 
 	public static Candidate fromString(String str)
 			throws SeatAllocationException {
-		String[] parts = str.split(":");
 
-		if (parts.length != 2)
-			throw new SeatAllocationException(
-					"Candidate must be of the form Name:votes");
+		StringTokenizer st = new StringTokenizer(str, ":=");
+
+		String name = st.nextToken();
+		String votes = st.nextToken();
 
 		Candidate candidate;
 
 		try {
-			candidate = new Candidate(parts[0]);
-			candidate.setVotes(Integer.parseInt(parts[1]));
+			candidate = new Candidate(name);
+			candidate.setVotes(Integer.parseInt(votes));
+
+			while (st.countTokens() >= 2) {
+				candidate.setProperty(st.nextToken(), st.nextToken());
+			}
 
 		} catch (Exception e) {
 			throw new SeatAllocationException(
-					"Candidate must be of the form Name:votes: "
+					"Candidate must be of the form Name:votes[:key=value] "
 							+ e.getMessage());
 		}
+
 		return candidate;
 	}
 }
