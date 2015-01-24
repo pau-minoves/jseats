d694bdf1fb3b5d1c9e61d968ef5ef9acf855551e
alvaro garcia <derecerca@gmail.com>
2015-01-24 11:27:06 +0100

    green: create_a_tally_with_one_candidate

--- src/main/java/org/jseats/unit/TallyBuilder.java
+++ src/main/java/org/jseats/unit/TallyBuilder.java
@@ -1,16 +1,29 @@
 package org.jseats.unit;
 
+import org.jseats.model.Candidate;
 import org.jseats.model.Tally;
 
 /**
  * Created by alvaro on 24/01/15.
  */
 public class TallyBuilder {
+	private Candidate candidate;
+
 	public static TallyBuilder aNew() {
 		return new TallyBuilder();
 	}
 
 	public Tally build() {
-		return new Tally();
+		final Tally tally = new Tally();
+		if(null != candidate){
+			tally.addCandidate(candidate);
+		}
+		return tally;
+
+	}
+
+	public TallyBuilder with(Candidate candidate) {
+		this.candidate = candidate;
+		return this;
 	}
 }
--- src/test/java/org/jseats/unit/TallyBuilderShould.java
+++ src/test/java/org/jseats/unit/TallyBuilderShould.java
@@ -1,7 +1,24 @@
 package org.jseats.unit;
 
+import org.jseats.model.Candidate;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 /**
  * Created by alvaro on 24/01/15.
  */
 public class TallyBuilderShould {
+
+	@Test
+	public void create_a_tally_with_no_candidates() {
+		assertThat(TallyBuilder.aNew().build().getNumberOfCandidates(), is(0));
+	}
+
+	@Test
+	public void create_a_tally_with_one_candidate() {
+
+		assertThat(TallyBuilder.aNew().with(new Candidate()).build().getNumberOfCandidates(), is(1));
+	}
 }
